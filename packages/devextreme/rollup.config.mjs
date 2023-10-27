3c32ec708d609edfebef74e75b91e732a1a62134
Roman Semenov <roman.semenov@devexpress.com>
2023-10-27 13:25:22 +0400

    try

--- packages/devextreme/rollup.config.mjs
+++ packages/devextreme/rollup.config.mjs
@@ -4,6 +4,7 @@ import glob from 'glob';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import del from 'rollup-plugin-delete';
+import alias from '@rollup/plugin-alias';
 
 const jsInput = Object.fromEntries(
     glob.sync('js/**/*', {
@@ -29,26 +30,26 @@ const renovationInput = Object.fromEntries(
     glob.sync('js/renovation/dist/**/*', {
         nodir: true,
     }).map(file => [
-        path.join('renovation',
-            path.relative(
-                'js/renovation/dist',
-                file.slice(0, file.length - path.extname(file).length)
-            )
+        path.relative(
+            'js',
+            file.slice(0, file.length - path.extname(file).length)
         ),
         fileURLToPath(new URL(file, import.meta.url))
     ])
 );
 
 const input = {
+    // ...renovationInput,
     ...jsInput,
-    ...renovationInput
 }
+
+const inputByFiles = Object.fromEntries(Object.entries(input).map(([a, b]) => [b, a]));
 
 export default [{
     input,
     external: [
         /__internal/,
-        /renovation/
+        /renovation/,
     ],
     output: [
         {
@@ -60,10 +61,25 @@ export default [{
             format: 'cjs',
             dir: 'dist/cjs',
             preserveModules: true,
-        }
+        },
+        // {
+        //     format: 'packagejsonlink',
+        //     dir: 'dist',
+        //     preserveModules: true,
+        // }
     ],
     plugins: [
         del({ targets: 'dist/*' }),
+        {
+            name: 'a',
+            resolveId(source, importer, options) {
+                if (options.isEntry) {
+                    return;
+                }
+
+                console.log(source, importer, options);  
+            },
+        }
     ],
     treeshake: false
 }];
