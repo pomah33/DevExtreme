ff5813d3a1c29f6124c476105d49b39dccb8069a
Roman Semenov <roman.semenov@devexpress.com>
2024-02-25 18:34:16 +0400

    move to class

--- packages/devextreme/js/core/utils/deferred.js
+++ packages/devextreme/js/core/utils/deferred.js
@@ -2,147 +2,151 @@ import { isDeferred, isDefined, isPromise } from '../utils/type';
 import { extend } from '../utils/extend';
 import Callbacks from '../utils/callbacks';
 
-let DeferredObj = function() {
-    const that = this;
-    this._state = 'pending';
-    this._promise = {};
+let DeferredObj = class DeferredObj {
+    constructor() {
+        const that = this;
+        this._state = 'pending';
+        this._promise = {};
 
-    this.resolveCallbacks = Callbacks();
-    this.rejectCallbacks = Callbacks();
-    this.notifyCallbacks = Callbacks();
+        this.resolveCallbacks = Callbacks();
+        this.rejectCallbacks = Callbacks();
+        this.notifyCallbacks = Callbacks();
 
-    this.resolve = function() {
-        return this.resolveWith(this._promise, arguments);
-    }.bind(this);
-    this.reject = function() {
-        return this.rejectWith(this._promise, arguments);
-    }.bind(this);
-    this.notify = function() {
-        return this.notifyWith(this._promise, arguments);
-    }.bind(this);
+        this.resolve = function() {
+            return this.resolveWith(this._promise, arguments);
+        }.bind(this);
+        this.reject = function() {
+            return this.rejectWith(this._promise, arguments);
+        }.bind(this);
+        this.notify = function() {
+            return this.notifyWith(this._promise, arguments);
+        }.bind(this);
 
-    this._promise._handler = function(methodName, handler) {
-        if(!handler) return this;
+        this._promise._handler = function(methodName, handler) {
+            if(!handler) return this;
 
-        const callbacks = that[methodName + 'Callbacks'];
-        if(callbacks.fired()) {
-            handler.apply(that[methodName + 'Context'], that[methodName + 'Args']);
-        } else {
-            callbacks.add(function(context, args) {
-                handler.apply(context, args);
+            const callbacks = that[methodName + 'Callbacks'];
+            if(callbacks.fired()) {
+                handler.apply(that[methodName + 'Context'], that[methodName + 'Args']);
+            } else {
+                callbacks.add(function(context, args) {
+                    handler.apply(context, args);
+                }.bind(this));
+            }
+            return this;
+        };
+
+        this._promise.done = function(handler) {
+            return this._handler('resolve', handler);
+        };
+        this._promise.fail = function(handler) {
+            return this._handler('reject', handler);
+        };
+        this._promise.progress = function(handler) {
+            return this._handler('notify', handler);
+        };
+
+        this._promise.always = function(handler) {
+            return this.done(handler).fail(handler);
+        };
+
+        this._promise.catch = function(handler) {
+            return this.then(null, handler);
+        };
+
+        this._promise.then = function(resolve, reject) {
+            const result = new DeferredObj();
+
+            ['done', 'fail'].forEach(function(method) {
+                const callback = method === 'done' ? resolve : reject;
+
+                this[method](function() {
+                    if(!callback) {
+                        result[method === 'done' ? 'resolve' : 'reject'].apply(this, arguments);
+                        return;
+                    }
+
+                    const callbackResult = callback && callback.apply(this, arguments);
+                    if(isDeferred(callbackResult)) {
+                        callbackResult.done(result.resolve).fail(result.reject);
+                    } else if(isPromise(callbackResult)) {
+                        callbackResult.then(result.resolve, result.reject);
+                    } else {
+                        result.resolve.apply(this, isDefined(callbackResult) ? [callbackResult] : arguments);
+                    }
+                });
             }.bind(this));
-        }
-        return this;
-    };
 
-    this._promise.done = function(handler) {
-        return this._handler('resolve', handler);
-    };
-    this._promise.fail = function(handler) {
-        return this._handler('reject', handler);
-    };
-    this._promise.progress = function(handler) {
-        return this._handler('notify', handler);
-    };
+            return result.promise();
+        };
 
-    this._promise.always = function(handler) {
-        return this.done(handler).fail(handler);
-    };
+        this._promise.state = function() {
+            return that._state;
+        };
 
-    this._promise.catch = function(handler) {
-        return this.then(null, handler);
-    };
+        this._promise.promise = function(args) {
+            return args ? extend(args, that._promise) : that._promise;
+        };
 
-    this._promise.then = function(resolve, reject) {
-        const result = new DeferredObj();
+        this.done = function(handler) {
+            return this._promise.done.call(this, handler);
+        };
+        this.fail = function(handler) {
+            return this._promise.fail.call(this, handler);
+        };
+        this.progress = function(handler) {
+            return this._promise.progress.call(this, handler);
+        };
 
-        ['done', 'fail'].forEach(function(method) {
-            const callback = method === 'done' ? resolve : reject;
+        this.always = function(handler) {
+            return this._promise.always.call(this, handler);
+        };
 
-            this[method](function() {
-                if(!callback) {
-                    result[method === 'done' ? 'resolve' : 'reject'].apply(this, arguments);
-                    return;
-                }
+        this.catch = function(handler) {
+            return this._promise.catch.call(this, handler);
+        };
 
-                const callbackResult = callback && callback.apply(this, arguments);
-                if(isDeferred(callbackResult)) {
-                    callbackResult.done(result.resolve).fail(result.reject);
-                } else if(isPromise(callbackResult)) {
-                    callbackResult.then(result.resolve, result.reject);
-                } else {
-                    result.resolve.apply(this, isDefined(callbackResult) ? [callbackResult] : arguments);
-                }
-            });
-        }.bind(this));
+        this.then = function(resolve, reject) {
+            return this._promise.then.call(this, resolve, reject);
+        };
 
-        return result.promise();
-    };
+        this.state = function() {
+            return this._promise.state.call(this);
+        };
 
-    this._promise.state = function() {
-        return that._state;
-    };
+        this.promise = function(args) {
+            return this._promise.promise.call(this, args);
+        };
 
-    this._promise.promise = function(args) {
-        return args ? extend(args, that._promise) : that._promise;
-    };
-
-    this.done = function(handler) {
-        return this._promise.done.call(this, handler);
-    };
-    this.fail = function(handler) {
-        return this._promise.fail.call(this, handler);
-    };
-    this.progress = function(handler) {
-        return this._promise.progress.call(this, handler);
-    };
-
-    this.always = function(handler) {
-        return this._promise.always.call(this, handler);
-    };
-
-    this.catch = function(handler) {
-        return this._promise.catch.call(this, handler);
-    };
-
-    this.then = function(resolve, reject) {
-        return this._promise.then.call(this, resolve, reject);
-    };
-
-    this.state = function() {
-        return this._promise.state.call(this);
-    };
-
-    this.promise = function(args) {
-        return this._promise.promise.call(this, args);
-    };
-
-    this._handler = function(methodName, handler) {
-        return this._promise._handler.call(this, methodName, handler);
-    };
-};
-
-DeferredObj.prototype._methodWith = function(methodName, state, context, args) {
-    const callbacks = this[methodName + 'Callbacks'];
-
-    if(this.state() === 'pending') {
-        this[methodName + 'Args'] = args;
-        this[methodName + 'Context'] = context;
-        if(state) this._state = state;
-        callbacks.fire(context, args);
+        this._handler = function(methodName, handler) {
+            return this._promise._handler.call(this, methodName, handler);
+        };
     }
 
-    return this;
-};
+    _methodWith(methodName, state, context, args) {
+        const callbacks = this[methodName + 'Callbacks'];
 
-DeferredObj.prototype.resolveWith = function(context, args) {
-    return this._methodWith('resolve', 'resolved', context, args);
-};
-DeferredObj.prototype.rejectWith = function(context, args) {
-    return this._methodWith('reject', 'rejected', context, args);
-};
-DeferredObj.prototype.notifyWith = function(context, args) {
-    return this._methodWith('notify', undefined, context, args);
+        if(this.state() === 'pending') {
+            this[methodName + 'Args'] = args;
+            this[methodName + 'Context'] = context;
+            if(state) this._state = state;
+            callbacks.fire(context, args);
+        }
+
+        return this;
+    }
+
+    resolveWith(context, args) {
+        return this._methodWith('resolve', 'resolved', context, args);
+    }
+
+    rejectWith(context, args) {
+        return this._methodWith('reject', 'rejected', context, args);
+    }
+
+    notifyWith(context, args) {
+        return this._methodWith('notify', undefined, context, args);
+    }
 };
 
 export function fromPromise(promise, context) {
